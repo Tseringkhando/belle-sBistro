86dca76abcb5bc9be6742e3cb8a2042f5cd0563e
rabinapanta <rabinapanta999@gmail.com>
2024-04-16 19:51:58 -0400

    new commit

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -30,22 +30,31 @@ public class LoginController {
 	 * It fetches user inputs, validates them, and processes the login based on user type.
 	 */
 	public void loginData() {
+		// Add action listener to the login button in the view
 		view.addActionToBtn(new ActionListener () {
 
 			public void actionPerformed(ActionEvent e) {
+				// Print username to console (for debugging)
 				System.out.println(view.getUsername());
+				 // Check if the selected user type is "Admin"
 				if(view.getUserType().getSelectedItem().equals("Admin"))
 				{
-					model.setType("AD");
+					model.setType("AD"); // Set user type to "AD" (Admin)
+					 // Attempt to log in as an admin
 					model.loginAdmin();
+					 // Verify the username and password
 					if(model.verify(view.getUsername(), view.getUserPassword())) {
+						// If verification is successful, close the login view
 						view.dispose();
+						// Open the AdminView
 						AdminView adminView = new AdminView();
 						adminView.setVisible(true);
+						// Initialize the AdminController and check button permissions
 						AdminController adminController= new AdminController(adminView);
 						adminController.checkbutton();
 					}
 					else {
+						 // If verification fails, show an error message
 						JOptionPane.showMessageDialog(null, "Username or Password Incorrect!");
 					}
 				}
