d895aa8dd54042a0ed7c735ce0718fac6db7f80d
rabinapanta <rabinapanta999@gmail.com>
2024-04-16 18:41:50 -0400

    new commit

--- src/Model/Departments.java
+++ src/Model/Departments.java
@@ -3,6 +3,9 @@ package Model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * This class represents a department with a department ID and name.
+ */
 public class Departments implements Serializable {
 
 	/**
@@ -12,21 +15,33 @@ public class Departments implements Serializable {
 	
 	private String deptID, deptName;
 	ArrayList<Departments> d = new ArrayList<Departments>();
+	
+	// Parameterized constructor
 	public Departments(String id, String name)
 	{
 		setDeptID(id);
 		setDeptName(name);
 	}
+	
+	// Default constructor
 	public Departments() {}
+	
+	// Getter for department ID
 	public String getDeptID() {
 		return deptID;
 	}
+	
+	// Setter for department ID
 	public void setDeptID(String deptID) {
 		this.deptID = deptID;
 	}
+	
+	 // Getter for department name
 	public String getDeptName() {
 		return deptName;
 	}
+	
+	 // Setter for department name
 	public void setDeptName(String deptName) {
 		this.deptName = deptName;
 	}
