31da9c9facb6ddb0f6ca97d727058bc1ed706549
rabinapanta <rabinapanta999@gmail.com>
2024-04-17 01:09:28 -0400

    new commit

--- src/belleByte/Main.java
+++ src/belleByte/Main.java
@@ -5,13 +5,22 @@ import java.awt.*;
 import Controller.LoginController;
 import Model.Login;
 import View.*;
+
+/**
+ * The Main class serves as the entry point for the Human Resource Management System (HRMS) application.
+ * It creates an instance of the LoginView class to display the login interface, initializes the Login model,
+ * and sets up the LoginController to handle login attempts.
+ */
 public class Main {
 	public static void main(String[] args) {
-
+		// Create a new instance of the LoginView
         LoginView view = new LoginView();
         view.setVisible(true);
+        // Initialize the Login model
         Login model= new Login();
+        // Create a new LoginController with the LoginView and Login model
 		LoginController con = new LoginController(view, model);
+		// Call the loginData method in the controller to handle login attempts
 		con.loginData();
         
     }
